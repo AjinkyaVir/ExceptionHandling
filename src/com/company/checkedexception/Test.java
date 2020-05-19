a44560498450da100edbaff6696fb0fc66ca9dc4
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2020-05-19 20:59:17 +0530

    Ajinkya : Exception Handling checked Exception

---
+++ src/com/company/checkedexception/Test.java
@@ -0,0 +1,23 @@
+package com.company.checkedexception;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exception which are checked by compiler is called checked exception");
+        System.out.println("Exception which are child class of Exception class is called checked exception");
+
+        //Thread.sleep(1000); //java.lang.InterruptedException
+
+
+
+        //FileInputStream fileInputStream = new FileInputStream("abc.txt"); //java.io.FileNotFoundException
+
+
+
+    }
+
+}
